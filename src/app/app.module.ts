dcde9c6531bf6babae9ff98eaf5a4e79e0130481
jason-nordheim <jason.nordheim@gmail.com>
2021-01-09 15:38:06 -0700

    added new components for import into app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
+import { NodeComponent } from './grid/node/node.component';
+import { MonotonComponent } from './typography/monoton/monoton.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GridComponent
+    GridComponent,
+    NodeComponent,
+    MonotonComponent
   ],
   imports: [
     BrowserModule,
