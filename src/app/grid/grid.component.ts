79f8a627512feb7a1471c27f8e339a0c324a3b1f
jason-nordheim <jason.nordheim@gmail.com>
2021-01-09 16:28:53 -0700

    started stubbing out the Grid Component

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.scss']
+  styleUrls: ['./grid.component.scss'],
 })
 export class GridComponent implements OnInit {
+  @Input() rows: number = 40;
+  @Input() cols: number = 10;
 
-  constructor() { }
+  nodes: any[];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
