34ff96c6d2eb413d230d033e09d3fe5085c6b4cc
jason-nordheim <jason.nordheim@gmail.com>
2021-01-09 15:46:22 -0700

    changed to inline template

--- src/app/typography/monoton/monoton.component.ts
+++ src/app/typography/monoton/monoton.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-monoton',
-  templateUrl: './monoton.component.html',
-  styleUrls: ['./monoton.component.scss']
+  template: `<p>{{ text }}</p>`,
+  styles: [
+    `
+      @import url('https://fonts.googleapis.com/css2?family=Monoton&display=swap');
+
+      .monoton {
+        font-family: 'Monoton', cursive;
+        font-weight: 400; // only available font-weight for this font
+      }
+    `,
+  ],
 })
 export class MonotonComponent implements OnInit {
+  @Input() text: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
